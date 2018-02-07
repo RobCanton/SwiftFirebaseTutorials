5df525a162cb77867e6ec38a1f1599427b0c93a8
Robert Canton <robshanecanton@gmail.com>
2018-02-07 14:37:13 -0500

    Added Part 2 Completed Project

---
+++ FirebaseApp-2-User-Profiles/FirebaseApp/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  FirebaseApp
+//
+//  Created by Robert Canton on 2018-02-02.
+//  Copyright © 2018 Robert Canton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class HomeViewController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func handleLogout(_ sender:Any) {
+        try! Auth.auth().signOut()
+        self.dismiss(animated: false, completion: nil)
+    }
+}
---
+++ FirebaseApp-2-User-Profiles/FirebaseApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseApp
+//
+//  Created by Robert Canton on 2018-02-02.
+//  Copyright © 2018 Robert Canton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
