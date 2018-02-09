368560fdb4c1a30dc6714353b6fd080533e37cf1
Robert Canton <robshanecanton@gmail.com>
2018-02-08 20:12:33 -0500

    Added Autolayout project

---
+++ FirebaseApp-3-Autolayout-Start/FirebaseApp/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  FirebaseApp
+//
+//  Created by Robert Canton on 2018-02-02.
+//  Copyright © 2018 Robert Canton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class HomeViewController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func handleLogout(_ sender:Any) {
+        try! Auth.auth().signOut()
+    }
+}
